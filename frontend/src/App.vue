11a49b2267d3ffc31d61fe7a5404a2a769a0b836
Sean Cabalse <sean.cabalse@globe.com.ph>
2022-11-13 08:31:08 +0800

    Added web3 and metamask integ

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,15 @@
-<script setup>
-  import Game from './components/Game.vue'
-</script>
-
-
 <template>
-  <Game />
+  <div>
+    <Game />
+    <WalletConnect/>
+  </div>
+    
 </template>
 
 <style scoped>
 </style>
+
+<script setup>
+  import Game from './components/Game.vue'
+  import WalletConnect from './components/WalletConnect.vue'
+</script>
